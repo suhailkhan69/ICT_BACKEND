07ce0513958d51ec00f1033a471e18de5a9c48a4
suhailkhan69 <suhailoxfo@gmail.com>
2023-05-26 16:14:00 +0530

    pending

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const donor = require("./model/donor");
+const pending = require("./model/pending")
 require('dotenv').config()
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
@@ -23,14 +24,22 @@ app.get('/',function(req,res){
     res.send("hello");
 })
 app.post("/user/request",(req,res)=>{
-    var donors = new donor(req.body);//passing to DB
+    var donors = new pending(req.body);//passing to DB
     donors.save();//save to DB
     })
+    app.post("/admin/approve",(req,res)=>{
+        var donors = new donor(req.body);//passing to DB
+        donors.save();//save to DB
+        })
 
 app.get("/user/view",async (req,res)=>{
        var result = await donor.find({});
        res.json(result);
     })
+    app.get("/admin/pending/view",async (req,res)=>{
+        var result = await pending.find({});
+        res.json(result);
+     })
     app.use('/users', usersRouter)
     app.use('/login', loginRouter)
 
@@ -43,9 +52,12 @@ app.get("/user/view",async (req,res)=>{
         var result=await donor.findByIdAndDelete(req.body._id);
         res.send("deleted");
     })
-
+    app.post("/admin/pending/delete",async (req,res)=>{
+        var result=await pending.findByIdAndDelete(req.body._id);
+        res.send("deleted");
+    })
     const PORT = process.env.PORT || 6901
-    
+
 app.listen(6901,()=>{
     console.log("server running at port 6901")
 })
